09e26f49b8ce76f3b95c2cb48747bcb457cdf3d2
= <=>
2017-04-14 18:48:57 -0400

    initial

--- src/main/settings/contest.py
+++ src/main/settings/contest.py
@@ -11,10 +11,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dc08ksnl4p0ec6',
-        'USER': 'honmhaszhrchhb',
-        'PASSWORD': 'Pdi86c-z1WOS0XoXMOz22K0toK',
-        'HOST': 'ec2-54-243-249-165.compute-1.amazonaws.com',
+        'NAME': 'dc60t9t4dnghoh',
+        'USER': 'qdoswzvbrgmmtu',
+        'PASSWORD': '0141da0ef7bbab4c04aab682d5288869a2bc9c03569e91b2c8661595003a4a7f',
+        'HOST': 'ec2-54-221-254-72.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
