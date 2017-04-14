2ddfc656d82d5d833ffce0d1c2f90c89fc46a18f
= <=>
2017-04-14 19:41:59 -0400

    2017-04-14 19:41:59.633515

--- deploy.py
+++ deploy.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import os
+import subprocess
 import datetime as dt
 
 def pushGitHub():
-    date = str(dt.datetime.now())
-    os.system('git add .')
-    os.system('git commit -m "{0}"'.format(date))
-    os.system('git push origin master')
+    diff = subprocess.check_output(['git', 'diff'])
+    if diff:
+        date = str(dt.datetime.now())
+        os.system('git add .')
+        os.system('git commit -m "{0}"'.format(date))
+        os.system('git push origin master')
+    else:
+        print 'No hay diferencias'
 
 pushGitHub()
